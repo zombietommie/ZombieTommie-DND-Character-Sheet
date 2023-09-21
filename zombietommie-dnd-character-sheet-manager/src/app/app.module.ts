6ae9c7be01f869631a29aa31e0e349d606e2d1ec
Tommy A Tran <36934180+zombietommie@users.noreply.github.com>
2023-09-21 14:24:18 -0500

    reconfigured all over again

---
+++ zombietommie-dnd-character-sheet-manager/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { CharacterListComponent } from './character-list/character-list.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginpageComponent,
+    CharacterListComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
