e7d6e5fb1fd00f158606a09d4b6446a932ab33ab
Tommy <zombietommie@gmail.com>
2023-09-22 09:11:58 -0500

    making first code changes

--- zombietommie-dnd-character-sheet-manager/src/app/app.component.ts
+++ zombietommie-dnd-character-sheet-manager/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'zombietommie-dnd-character-sheet-manager';
-}
+export class AppComponent {}
