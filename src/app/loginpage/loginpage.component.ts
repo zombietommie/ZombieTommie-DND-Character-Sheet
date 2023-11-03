6f7595ecd33869edd398e2ee5d011a97581b5f25
Tommy <zombietommie@gmail.com>
2023-11-02 21:33:34 -0500

    trying to add input

--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loginpage',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./loginpage.component.css'],
 })
 export class LoginpageComponent {
+  @Input() loginResult = '';
   username: string = '';
   password: string = '';
   showSuccess: boolean = false;
